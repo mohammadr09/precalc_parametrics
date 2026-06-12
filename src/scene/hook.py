6024bebad40fbed51c6d21f8d6f337dd14425890
mohammadr09 <mohammadnyc96@gmail.com>
2026-06-11 22:53:01 -0400

    HookScene Design 2

--- src/scene/hook.py
+++ src/scene/hook.py
@@ -1,34 +1,50 @@
-from manim import * # type: ignore
+from manim import *
 
 class HookScene(Scene):
     def construct(self):
         circle = Circle()
-        ellipse = Ellipse(width = 4, height = 2)
+        ellipse = Ellipse(width=4, height=2)
+
         dot = Dot(circle.point_from_proportion(0))
 
         self.play(Create(circle))
-        self.play(Create(dot))
+        self.play(FadeIn(dot))
         self.play(MoveAlongPath(dot, circle))
+
+        self.play(FadeOut(dot))
+        self.wait(0.3)
         
+        self.play(ReplacementTransform(circle, ellipse))
 
-        self.play(
-            ReplacementTransform(circle, ellipse),
-            dot.animate.move_to(ellipse.point_from_proportion(0))
-        )
+        dot.move_to(ellipse.point_from_proportion(0))
 
+        self.play(FadeIn(dot))
         self.play(MoveAlongPath(dot, ellipse))
 
-        hyperbola = ParametricFunction(
-            lambda t: np.array([
-                2*np.cosh(t),
-                np.sinh(t),
-                0
-            ]),
-            t_range=(-1.5, 1.5)
+        self.play(FadeOut(dot))
+        self.wait(0.3)
+
+        plane = NumberPlane(
+            x_range=[-8, 8, 1],
+            y_range=[-5, 5, 1],
+            x_length=8,
+            y_length=5,
         )
 
-        self.play(
-            ReplacementTransform(ellipse, hyperbola),
-            dot.animate.move_to(hyperbola.point_from_proportion(0))
+        labels = plane.get_axis_labels("x", "y")
+
+        hyperbola = ParametricFunction(
+            lambda t: plane.c2p([
+                2*np.cosh(t),
+                np.sinh(t)
+            ]),
+            t_range=(-2, 2)
         )
+
+        self.add(plane, labels)
+        self.play(ReplacementTransform(ellipse, hyperbola))
+
+        dot.move_to(hyperbola.point_from_proportion(0))
+
+        self.play(FadeIn(dot))
         self.play(MoveAlongPath(dot, hyperbola))
